a7309a7225415fc534208cda29666fdf6eec5eaa
An Tran <tran.binhan@gmail.com>
2021-05-16 16:28:37 +0800

    Saved

--- libs/rpi-rfid-video/bplay_makerphat.py
+++ libs/rpi-rfid-video/bplay_makerphat.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+
+import sys
 from random import randint
 import SimpleMFRC522
 import time
@@ -7,8 +9,7 @@ import os
 import logging
 import random
 import glob
-import RPi.GPIO as GPIO
-from gpiozero import Buzzer
+from gpiozero import LED, Button, Buzzer
 
 is_starting_player = False
 
@@ -21,8 +22,7 @@ def waitForPlayer():
     global is_starting_player
 
     while True:
-        if isPlayerPlaying():
-            break
+        if isPlayerPlaying(): break
         print("Waiting for player to start ....")
         time.sleep(3)
 
@@ -31,70 +31,64 @@ def waitForPlayer():
 
 def playmovie(video, aspect = 0):
 
-	"""plays a video."""
+    """plays a video."""
 
-	global myprocess
-	global directory
-        global is_starting_player
+    global myprocess
+    global directory
+    global is_starting_player
 
-	logging.debug('linux: omxplayer %s' % video)
+    logging.debug('linux: omxplayer %s' % video)
 
-	proccount = isplaying()
+    proccount = isplaying()
 
-	if proccount == 1 or proccount == 0:
+    if proccount == 1 or proccount == 0:
+        logging.debug('No videos playing, so play video')
 
-		logging.debug('No videos playing, so play video')
-
-	else:
-
-		logging.debug('Video already playing, so quit current video, then play')
-		myprocess.communicate(b"q")
-		
-	if aspect == 0:
-		myprocess = subprocess.Popen(['omxplayer','-o', 'hdmi', directory + video],stdin=subprocess.PIPE,stdout=subprocess.PIPE,stderr=subprocess.PIPE, close_fds=True)
-	
-	else:
-	#This is for videos that come in 16:9 that should be played as 4:3
-	#if your video file is already in 4:3, you don't need to set this flag.
-		myprocess = subprocess.Popen(['omxplayer','--win','250,0,1650,1050',directory + video],stdin=subprocess.PIPE,stdout=subprocess.PIPE,stderr=subprocess.PIPE, close_fds=True)
-	
+    else:
+        logging.debug('Video already playing, so quit current video, then play')
+        myprocess.communicate(b"q")
+        
+    if aspect == 0:
+        myprocess = subprocess.Popen(['omxplayer','-o', 'hdmi', directory + video],stdin=subprocess.PIPE,stdout=subprocess.PIPE,stderr=subprocess.PIPE, close_fds=True)
+    
+    else:
+        #This is for videos that come in 16:9 that should be played as 4:3
+        #if your video file is already in 4:3, you don't need to set this flag.
+        myprocess = subprocess.Popen(['omxplayer','--win','250,0,1650,1050',directory + video],stdin=subprocess.PIPE,stdout=subprocess.PIPE,stderr=subprocess.PIPE, close_fds=True)
         waitForPlayer()
 
 def playaudio(audio):
 
-	"""plays an audio."""
+    """plays an audio."""
 
-	global myprocess
-	global directory
+    global myprocess
+    global directory
 
-	logging.debug('linux: omxplayer -o hdmi %s' % audio)
+    logging.debug('linux: omxplayer -o hdmi %s' % audio)
 
-	proccount = isplaying()
+    proccount = isplaying()
 
-	if proccount == 1 or proccount == 0:
+    if proccount == 1 or proccount == 0:
+        logging.debug('Player is not active, so start the player now')
 
-		logging.debug('Player is not active, so start the player now')
+    else:
+        logging.debug('Player is active, so quit current player, then play')
+        myprocess.communicate(b"q")
+        
+    myprocess = subprocess.Popen(['omxplayer','-o','hdmi',directory + audio],stdin=subprocess.PIPE,stdout=subprocess.PIPE,stderr=subprocess.PIPE, close_fds=True)
 
-	else:
-
-		logging.debug('Player is active, so quit current player, then play')
-		myprocess.communicate(b"q")
-		
-	myprocess = subprocess.Popen(['omxplayer','-o','hdmi',directory + audio],stdin=subprocess.PIPE,stdout=subprocess.PIPE,stderr=subprocess.PIPE, close_fds=True)
-
-        waitForPlayer()
+    waitForPlayer()
 
 def isplaying():
+    """check if omxplayer is running
+    if the value returned is a 1 or 0, omxplayer is NOT playing a video
+    if the value returned is a 2, omxplayer is playing a video"""
 
-		"""check if omxplayer is running
-		if the value returned is a 1 or 0, omxplayer is NOT playing a video
-		if the value returned is a 2, omxplayer is playing a video"""
+    processname = 'omxplayer'
+    tmp = os.popen("ps -Af").read()
+    proccount = tmp.count(processname)
 
-		processname = 'omxplayer'
-		tmp = os.popen("ps -Af").read()
-		proccount = tmp.count(processname)
-
-		return proccount
+    return proccount
 
 def beep(buzzer):
     buzzer.on()
@@ -106,15 +100,27 @@ def repeatBeep(buzzer, times):
     for i in range(times):
         beep(buzzer)
 
-        
+def cleanup(): #(signal, frame): 
+    print("end of program")
+    sys.exit(0)
+
+canExit=False
+
+def onButtonPress():
+    global canExit
+    print("button pressed")
+    canExit = True
+    print("end of logic")
+
 #program start
 
 logging.basicConfig(level=logging.DEBUG)
 
-GPIO.setmode(GPIO.BCM)
-
 reader = SimpleMFRC522.SimpleMFRC522()
 buzzer = Buzzer(26)
+stopButton = Button(20)
+
+stopButton.when_pressed=onButtonPress
 
 directory = '/home/pi/Videos/'
 
@@ -123,82 +129,67 @@ repeatBeep(buzzer, 2)
 print("Begin Player")
 
 try:
-	while True: 
-                if is_starting_player: continue
+    while not(canExit): 
+        # Do not read RFID cards when starting the player
+        if is_starting_player : continue
 
-		proccount = isplaying()
+        proccount = isplaying()
 
-		if proccount == 1 or proccount == 0:
+        if proccount == 1 or proccount == 0:
+            current_movie_id = long(10)
+            
+        start_time = time.time()
 
-			current_movie_id = long(10)
-			
-		start_time = time.time()
+        logging.debug("Waiting for ID to be scanned")
+        id, movie_name = reader.read()
 
-		logging.debug("Waiting for ID to be scanned")
-		id, movie_name = reader.read()
+        # Acknowledge the id read
+        beep(buzzer)
 
-                # Acknowledge the id read
-                beep(buzzer)
+        logging.debug("ID: %s" % id)
+        logging.debug("Movie Name: %s" % movie_name)
 
-		logging.debug("ID: %s" % id)
-		logging.debug("Movie Name: %s" % movie_name)
+        movie_name = movie_name.rstrip()
 
-		movie_name = movie_name.rstrip()
+        if current_movie_id != id:
+            is_starting_player = True
 
-		if current_movie_id != id:
-                        is_starting_player = True
+            logging.debug('New Movie')
+            # this is a check in place to prevent omxplayer from restarting video if ID is left over the reader.
+            # better to use id than movie_name as there can be a problem reading movie_name occasionally
+            
+            if movie_name.endswith(('.mp4', '.avi', '.m4v','.mkv')):
+                current_movie_id = id 	#we set this here instead of above bc it may mess up on first read
+                logging.debug("playing: omxplayer -o hdmi %s" % movie_name)
+                playmovie(movie_name)
 
-			logging.debug('New Movie')
-			#this is a check in place to prevent omxplayer from restarting video if ID is left over the reader.
-			#better to use id than movie_name as there can be a problem reading movie_name occasionally
-			
+            elif movie_name.endswith(('.mp3')):
+                current_movie_id = id 	#we set this here instead of above bc it may mess up on first read
+                logging.debug("playing: omxplayer -o hdmi %s" % movie_name)
+                playaudio(movie_name)
 
-			if movie_name.endswith(('.mp4', '.avi', '.m4v','.mkv')):
-				current_movie_id = id 	#we set this here instead of above bc it may mess up on first read
-				logging.debug("playing: omxplayer -o hdmi %s" % movie_name)
-				playmovie(movie_name)
+            elif 'folder' in movie_name:
+            #randomly plays video files from a certain folder
+                current_movie_id = id
+                movie_directory = movie_name.replace('folder',"") 
+                movie_name = random.choice(glob.glob(os.path.join(directory + movie_directory, '*')))
+                movie_name = movie_name.replace(directory,"")
 
-			elif movie_name.endswith(('.mp3')):
-				current_movie_id = id 	#we set this here instead of above bc it may mess up on first read
-				logging.debug("playing: omxplayer -o hdmi %s" % movie_name)
-				playaudio(movie_name)
+                logging.debug("randomly playing: omxplayer %s" % movie_name)
+                playmovie(movie_name)
+    
+        else:
+            end_time = time.time()
+            elapsed_time = end_time - start_time
+            proccount = isplaying()
 
-			elif 'folder' in movie_name:
-			#randomly plays video files from a certain folder
-				current_movie_id = id
-				movie_directory = movie_name.replace('folder',"") 
-				movie_name = random.choice(glob.glob(os.path.join(directory + movie_directory, '*')))
-				movie_name = movie_name.replace(directory,"")
-
-				logging.debug("randomly playing: omxplayer %s" % movie_name)
-				playmovie(movie_name)
-
-
-			elif 'fourthree' in movie_name:
-			#video files randomly played from a folder and that should be played in 4:3 aspect ratio
-				current_movie_id = id
-				movie_directory = movie_name.replace('fourthree',"")
-				movie_name = random.choice(glob.glob(os.path.join(directory + movie_directory, '*')))
-				movie_name = movie_name.replace(directory,"")
-
-				logging.debug("randomly playing: omxplayer %s" % movie_name)
-				playmovie(movie_name,1)
-	
-		else:
-
-			end_time = time.time()
-			elapsed_time = end_time - start_time
-			proccount = isplaying()
-
-			if proccount != 1 and proccount != 0:
-
-				if elapsed_time > 0.6:
-					#pause, unpause movie
-
-					logging.debug('Pausing movie - or - Playing movie')
-					myprocess.stdin.write("p")
-
+            if isPlayerPlaying():
+                if elapsed_time > 0.6:
+                    # pause, unpause movie
+                    logging.debug('Pausing movie - or - Playing movie')
+                    myprocess.stdin.write("p")
 
 except KeyboardInterrupt:
-	GPIO.cleanup()
-	print("\nAll Done")
+    print("\nInterrupted by keyboard")
+
+cleanup()
